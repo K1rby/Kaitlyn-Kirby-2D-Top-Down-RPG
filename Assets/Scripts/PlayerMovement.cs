e8cb76058e172b4d0e8b5bd99608ca22e818dd48
Kaitlyn <diabolopersona@gmail.com>
2021-11-17 16:24:18 +1100

    Project

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public Rigidbody2D rb2D;
+
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovementInput();
+    }
+
+    private void FixedUpdate()
+    {
+        rb2D.velocity = movement * moveSpeed;
+    }
+
+    void MovementInput()
+    {
+        float mx = Input.GetAxisRaw("Horizontal");
+        float my = Input.GetAxisRaw("Vertical");
+
+        movement = new Vector2(mx, my).normalized;
+    }
+}
