23327fc11776f32c8c2e600d55ea827ebf4b139e
Kaitlyn <diabolopersona@gmail.com>
2021-12-01 16:25:27 +1100

    Bow & Arrow Combat

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -1,18 +1,87 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerAttack : MonoBehaviour
 {
+    [SerializeField] GameObject ArrowPrefab;
+    [SerializeField] SpriteRenderer ArrowGFX;
+    [SerializeField] Slider bowPowerSlider;
+    [SerializeField] Transform Bow;
+
+    [Range(0, 10)]
+    [SerializeField] float bowPower;
+
+    [Range(0, 3)]
+    [SerializeField] float maxBowCharge;
+
+    float bowCharge;
+    bool canFire = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        bowPowerSlider.value = 0f;
+        bowPowerSlider.maxValue = maxBowCharge;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButton(0) && canFire)
+        {
+            ChargeBow();
+        }
+        else if (Input.GetMouseButtonUp(0) && canFire)
+        {
+            FireArrow();
+        }
+        else
+        {
+            if(bowCharge > 0f)
+            {
+                bowCharge -= 1f * Time.deltaTime;
+            }
+            else
+            {
+                bowCharge = 0f;
+                canFire = true;
+            }
+
+            bowPowerSlider.value = bowCharge;
+        }
+    }
+
+    void ChargeBow()
+    {
+        ArrowGFX.enabled = true;
+        bowCharge += Time.deltaTime;
+
+        bowPowerSlider.value = bowCharge;
+
+        if(bowCharge > maxBowCharge)
+        {
+            bowPowerSlider.value = maxBowCharge;
+        }
+    }
+
+    void FireArrow()
+    {
+        if (bowCharge > maxBowCharge)
+        {
+            bowCharge = maxBowCharge;
+        }
+
+        float arrowSpeed = bowCharge + bowPower;
+        float angle = Utility.AngleTowardsMouse(Bow.position);
+
+        Quaternion rot = Quaternion.Euler(new Vector3(0f, 0f, angle - 90f));
+
+        Arrow arrow = Instantiate(ArrowPrefab, Bow.position, rot).GetComponent<Arrow>();
+        arrow.arrowVelocity = arrowSpeed;
+
+        canFire = false;
+        ArrowGFX.enabled = false;
     }
 }
